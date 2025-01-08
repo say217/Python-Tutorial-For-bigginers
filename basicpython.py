b18f7109f6e591ac934dc945d77e402e923a2336
sayak samanta <sayaksmnt@gmail.com>
2025-01-08 07:07:46 +0530

    Update basicpython.py

--- basicpython.py
+++ basicpython.py
@@ -1643,12 +1643,36 @@ for x in (car1, boat1, plane1):
 
 
 
+def myfunc():
+  x = 300
+  print(x)
+
+myfunc()
+
+
+
+
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
 
 
 
 
 
 
+x = 300
+
+def myfunc():
+  print(x)
+
+myfunc()
+
+print(x)
 
 
 
