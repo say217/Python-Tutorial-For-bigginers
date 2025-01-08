218d16871de0d7605bed9d1c6e2ea5ab406f289f
sayak samanta <sayaksmnt@gmail.com>
2025-01-08 07:08:44 +0530

    Update basicpython.py

--- basicpython.py
+++ basicpython.py
@@ -1682,6 +1682,43 @@ print(x)
 
 
 
+x = 300
+
+def myfunc():
+  x = 200
+  print(x)
+
+myfunc()
+
+print(x)
+
+
+
+
+
+
+
+def myfunc():
+  global x
+  x = 300
+
+myfunc()
+
+print(x)
+
+
+
+
+
+x = 300
+
+def myfunc():
+  global x
+  x = 200
+
+myfunc()
+
+print(x)
 
 
 
@@ -1691,22 +1728,15 @@ print(x)
 
 
 
+def myfunc1():
+  x = "Jane"
+  def myfunc2():
+    nonlocal x
+    x = "hello"
+  myfunc2()
+  return x
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(myfunc1())
 
 
 
